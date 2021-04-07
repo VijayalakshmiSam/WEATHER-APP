85d76dac47a937de6fbfb7af240d7d969c82fb09
Vijayalakshmi Sampath <61058491+VijayalakshmiSam@users.noreply.github.com>
2021-04-07 16:18:18 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
 import React,{Component} from 'react'
-import PostList from './components/postlist';
-//import PostForm from './components/postform';
-import PostApiCall from './components/postapicall'
 import Home from './components/APICALL' 
-import Home1 from './components/fetchweather'
+
 
 class App extends Component 
 {
@@ -13,9 +9,7 @@ class App extends Component
   {
     return(
       <div className="App">
-          <PostApiCall/> 
-        {/* <PostList/> */}
-        {/* <Home/> */}
+            <Home/>
       </div>
     )
   }
