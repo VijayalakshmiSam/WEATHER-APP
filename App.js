d7804d82b91e0dd4acbb053f79227834cab51229
Vijayalakshmi Sampath <61058491+VijayalakshmiSam@users.noreply.github.com>
2021-04-09 18:26:37 +0530

    Update App.js

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React,{Component} from 'react'
 import Home from './components/APICALL' 
 import UserApiCall from './components/USERAPICALL'
+import UserHook from './components/apicallusinghook'
 
 class App extends Component 
 {
@@ -10,6 +11,7 @@ class App extends Component
     return(
       <div className="App">
             <UserApiCall/>
+            <UserHook/>
       </div>
     )
   }
